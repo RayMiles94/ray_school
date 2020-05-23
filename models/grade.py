d1535d746b51be95baa774efd627a1fb9f56bc97
raouf-slnee <raouf.jabri@slnee.com>
2020-05-23 13:18:51 +0100

    edit exam_type and grade models

--- models/grade.py
+++ models/grade.py
@@ -1,5 +1,5 @@
 from odoo import fields, api, models, _
-
+from  odoo.exceptions import ValidationError
 
 class garde(models.Model):
     _name = 'ray.school.grade'
@@ -7,3 +7,14 @@ class garde(models.Model):
 
     name = fields.Char(string="Grade Name", required=True)
     desc = fields.Char(string="Description", required=True)
+
+
+    @api.constrains('name')
+    def check_name(self):
+        if(len(self.name)>45):
+            raise ValidationError(_('name must less than 50 chars'))
+
+    @api.constrains('desc')
+    def check_name(self):
+        if (len(self.desc) > 45):
+            raise ValidationError(_('desc must less than 50 chars'))
