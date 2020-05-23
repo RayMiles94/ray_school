6a854e4da5a3ffdf8ffa58ceefbbee88fa13c45f
raouf-slnee <raouf.jabri@slnee.com>
2020-05-23 13:39:46 +0100

    edit attendance

--- models/attendance.py
+++ models/attendance.py
@@ -1,8 +1,11 @@
 from odoo import fields, api, models, _
+from odoo.exceptions import ValidationError
 
 
 class attendance(models.Model):
     _name = 'ray.school.attendance'
     _description = 'attendance'
 
-    name = fields.Char()
+    student_id = fields.Many2one('ray.school.student', string="Student", required=True)
+    status = fields.Boolean(string="Status", required=True)
+    remark = fields.Text(string="Remark")
